e32b5134a9aa79f152c30026b113af17f08e15bf
Akhila Chary <maheshwaramakhila03@gmail.com>
2025-11-17 18:43:22 +0530

    Implemented addition method

---
+++ src/com/calculator/Addition.java
@@ -0,0 +1,29 @@
+package com.calculator;
+
+public class Addition implements Calculator{
+
+	@Override
+	public int add(int a, int b) {
+		
+		return a+b;
+	}
+
+	@Override
+	public int sub(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int mul(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int div(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
